1629f1f8efd51b88e8966845983eed36e08baf1b
bahaa-20 <b.zyood999@gmail.com>
2024-08-26 18:05:19 +0300

    Adjusted the single blog post page V1

--- src/pages/en/blog-detail.js
+++ src/pages/en/blog-detail.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import Layout from "../../components/layout";
 import "../../components/style/BlogDetail.css";
 import FooterCon from "../../components/FooterCon";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 import Seo from "../../components/seo";
 import { useLocation } from "@reach/router";
 
@@ -24,7 +24,8 @@ const BlogDetail = () => {
         `https://strapi.softylus.com/api/blogs?filters[Slug][$eq]=${slug}&populate=*&locale=en`,
         {
           headers: {
-            Authorization: "Bearer e9279a95db02d9220f944a52d6c0288bb38c733eca16bef5ed2e634e7c53b043560a00b4793f333cec78a9f2f63b72b40288a527d1ed8fbe47a7d1a08f66a60d64762c85f43b5eeeeb50f38244490e6fe7f3e338b4263eaf18056e0f2eded7cf6b09542910930be55000e4205e764bea8933db3694e33722520774fb00e422cd",
+            Authorization:
+              "Bearer e9279a95db02d9220f944a52d6c0288bb38c733eca16bef5ed2e634e7c53b043560a00b4793f333cec78a9f2f63b72b40288a527d1ed8fbe47a7d1a08f66a60d64762c85f43b5eeeeb50f38244490e6fe7f3e338b4263eaf18056e0f2eded7cf6b09542910930be55000e4205e764bea8933db3694e33722520774fb00e422cd",
           },
         }
       );
@@ -41,13 +42,20 @@ const BlogDetail = () => {
             url: blogData.attributes.Featured_Image?.data
               ? `https://strapi.softylus.com${blogData.attributes.Featured_Image.data.attributes.url}`
               : "/default-image.jpg",
-            alt: blogData.attributes.Featured_Image?.data?.attributes?.alternativeText || blogData.attributes.Title,
+            alt:
+              blogData.attributes.Featured_Image?.data?.attributes
+                ?.alternativeText || blogData.attributes.Title,
           },
           author: blogData.attributes.Author,
           publicationDate: blogData.attributes.createdAt,
           updatedDate: blogData.attributes.updatedAt,
-          categories: blogData.attributes.categories?.data.map(category => category.attributes.Category) || [],
-          tags: blogData.attributes.tags?.data.map(tag => tag.attributes.Tag) || [],
+          categories:
+            blogData.attributes.categories?.data.map(
+              (category) => category.attributes.Category
+            ) || [],
+          tags:
+            blogData.attributes.tags?.data.map((tag) => tag.attributes.Tag) ||
+            [],
           seoTitle: blogData.attributes.SEO_Title,
           canonicalUrl: blogData.attributes.Canonical_URL,
           focusKeyword: blogData.attributes.Focus_Keyword,
@@ -61,17 +69,18 @@ const BlogDetail = () => {
   const fetchOtherBlogs = useCallback(async () => {
     try {
       const response = await axios.get(
-        `https://strapi.softylus.com/api/blogs?populate=*&locale=en&pagination[limit]=3`,
+        `https://strapi.softylus.com/api/blogs?populate=*&locale=en&pagination`,
         {
           headers: {
-            Authorization: "Bearer e9279a95db02d9220f944a52d6c0288bb38c733eca16bef5ed2e634e7c53b043560a00b4793f333cec78a9f2f63b72b40288a527d1ed8fbe47a7d1a08f66a60d64762c85f43b5eeeeb50f38244490e6fe7f3e338b4263eaf18056e0f2eded7cf6b09542910930be55000e4205e764bea8933db3694e33722520774fb00e422cd",
+            Authorization:
+              "Bearer e9279a95db02d9220f944a52d6c0288bb38c733eca16bef5ed2e634e7c53b043560a00b4793f333cec78a9f2f63b72b40288a527d1ed8fbe47a7d1a08f66a60d64762c85f43b5eeeeb50f38244490e6fe7f3e338b4263eaf18056e0f2eded7cf6b09542910930be55000e4205e764bea8933db3694e33722520774fb00e422cd",
           },
         }
       );
 
       const otherBlogsData = response.data.data
-        .filter(blogData => blogData.attributes.Slug !== slug)
-        .map(blogData => ({
+        .filter((blogData) => blogData.attributes.Slug !== slug)
+        .map((blogData) => ({
           id: blogData.id,
           title: blogData.attributes.Title,
           slug: blogData.attributes.Slug,
@@ -80,7 +89,9 @@ const BlogDetail = () => {
             url: blogData.attributes.Featured_Image?.data
               ? `https://strapi.softylus.com${blogData.attributes.Featured_Image.data.attributes.url}`
               : "/default-image.jpg",
-            alt: blogData.attributes.Featured_Image?.data?.attributes?.alternativeText || blogData.attributes.Title,
+            alt:
+              blogData.attributes.Featured_Image?.data?.attributes
+                ?.alternativeText || blogData.attributes.Title,
           },
         }));
 
@@ -98,23 +109,29 @@ const BlogDetail = () => {
   const convertContentToHtml = (content) => {
     return content.map((item, index) => {
       switch (item.type) {
-        case 'heading':
+        case "heading":
           return React.createElement(
-            `h${item.level}`, 
-            { key: index }, 
-            item.children.map(child => child.text).join('')
+            `h${item.level}`,
+            { key: index },
+            item.children.map((child) => child.text).join("")
           );
-        case 'paragraph':
+        case "paragraph":
           return (
             <p key={index}>
               {item.children.map((child, childIndex) => {
-                if (child.type === 'text') {
+                if (child.type === "text") {
                   let text = child.text;
-                  return child.bold ? <strong key={childIndex}>{text}</strong> : text;
-                } else if (child.type === 'link') {
+                  return child.bold ? (
+                    <strong key={childIndex}>{text}</strong>
+                  ) : (
+                    text
+                  );
+                } else if (child.type === "link") {
                   return (
                     <a key={child.url} href={child.url}>
-                      {child.children.map(linkChild => linkChild.text).join('')}
+                      {child.children
+                        .map((linkChild) => linkChild.text)
+                        .join("")}
                     </a>
                   );
                 }
@@ -122,19 +139,25 @@ const BlogDetail = () => {
               })}
             </p>
           );
-        case 'list':
-          const ListTag = item.format === 'ordered' ? 'ol' : 'ul';
+        case "list":
+          const ListTag = item.format === "ordered" ? "ol" : "ul";
           return (
             <ListTag key={index}>
               {item.children.map((li, liIndex) => (
                 <li key={liIndex}>
-                  {li.children.map((child, childIndex) => {
-                    if (child.type === 'text') {
-                      let text = child.text;
-                      return child.bold ? <strong key={childIndex}>{text}</strong> : text;
-                    }
-                    return null; // Ignore non-text children
-                  }).reduce((prev, curr) => [prev, ' ', curr])}
+                  {li.children
+                    .map((child, childIndex) => {
+                      if (child.type === "text") {
+                        let text = child.text;
+                        return child.bold ? (
+                          <strong key={childIndex}>{text}</strong>
+                        ) : (
+                          text
+                        );
+                      }
+                      return null; // Ignore non-text children
+                    })
+                    .reduce((prev, curr) => [prev, " ", curr])}
                 </li>
               ))}
             </ListTag>
@@ -150,36 +173,67 @@ const BlogDetail = () => {
 
   const htmlContent = convertContentToHtml(blog.content);
 
+  console.log("Others", otherBlogs);
+
   return (
     <Layout>
-      <Seo 
-        title={blog.seoTitle || blog.title}
-        description={blog.description}
-      />
+      <Seo title={blog.seoTitle || blog.title} description={blog.description} />
       <section className="md:px-[70px] mx-auto flex justify-center flex-col items-center pl-4 pr-4 mt-20">
         <div className="blog-container">
           <div className="show-post pl-4 pr-4">
-            <div className="blog-metadata">
+            <h1 className="single-post-title mt-4 md:mt-0">{blog.title}</h1>
+            {/* <div className="blog-metadata">
               <p>Categories: {blog.categories.join(", ")}</p>
               <p>Tags: {blog.tags.join(", ")}</p>
-            </div>
-            <img src={blog.featuredImage.url} alt={blog.featuredImage.alt} className="desktop"/>
+            </div> */}
+            <img
+              src={blog.featuredImage.url}
+              alt={blog.featuredImage.alt}
+              className="desktop"
+            />
             <div className="blog-card-big-content">
-              <h1 className="mt-4 md:mt-0">{blog.title}</h1>
-              <img src={blog.featuredImage.url} alt={blog.featuredImage.alt} className="mobile"/>
-              <div className="mt-4 line-height-p dangerouslySetInnerHTML">
+              <img
+                src={blog.featuredImage.url}
+                alt={blog.featuredImage.alt}
+                className="mobile"
+              />
+              <div className="mt-4 line-height-p dangerouslySetInnerHTML current-post-text">
                 {htmlContent}
               </div>
             </div>
           </div>
-          <div className="blog-list pl-4 pr-4 pb-6">
-            {otherBlogs.map(blog => (
-              <div key={blog.id} className="blog-card-list" onClick={() => window.location.href = `/blog-detail/?slug=${blog.slug}`}>
-                <img src={blog.featuredImage.url} alt={blog.featuredImage.alt} />
+        </div>
+        <div className="blog-list pl-4 pr-4 pb-6">
+          <div className="heading">
+            <h2>Similar posts to read</h2>
+            <hr />
+          </div>
+          <div className="blog-posts-container">
+            {otherBlogs?.map((otherPost) => (
+              <div
+                key={otherPost.id}
+                className="blog-card-list"
+                onClick={() =>
+                  (window.location.href = `/blog-detail/?slug=${otherPost.slug}`)
+                }
+              >
+                <img
+                  src={otherPost.featuredImage.url}
+                  alt={otherPost.featuredImage.alt}
+                />
                 <div className="blog-card-list-content">
-                  <h2 className="clamp-3-lines md:font-normal font-normal-css">{blog.title}</h2>
+                  <div className="post-info">
+                    <div className="post-categories">
+                      <p>{otherPost.categories?.[0] || "Category"}</p>
+                    </div>
+                    <span>—</span>
+                    <p className="post-date">
+                      {otherPost.publicationDate || "November, 6 2023"}
+                    </p>
+                  </div>
+                  <p className="post-title">{otherPost?.title}</p>
                   <div className="blog-description">
-                    {/* Truncate description logic here */}
+                    {convertContentToHtml(otherPost.content)}
                   </div>
                 </div>
               </div>
